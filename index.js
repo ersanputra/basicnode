faee1c38781a910cdfbf2690bcf736415101a21f
ersanputra <ersancakra@gmail.com>
2023-09-01 21:53:07 +0700

    push get and post http

--- index.js
+++ index.js
@@ -1,20 +1,65 @@
 const MamaliaController = require("./class/controllers/mamalia.controller");
 
-async function main() {
-    try {
-        const mamalia = { name: "Lumba-lumba", type: "Mamalia", habitat: "Laut" } 
-        const mamaliaController = new MamaliaController();
-        mamaliaController.getSuara();
+//ini import module HTTP
+const http = require("http");
 
-        await mamaliaController.store(mamalia);
+const server = http.createServer(async(req, res) => {
+    const method = req.method;
+    const mamaliaController = new MamaliaController();
+    switch (method) {
+        case "GET":
+            const mamalias = await mamaliaController.getAll();
+            //console.log(mamalias);
+            res.setHeader("Content-Type", "application/json")
+            res.end(JSON.stringify(mamalias, null, 2))
+            break;
 
-        const mamalias = await mamaliaController.getAll();
-        console.log(mamalias);
-    } catch (error) {
-        console.log(error);
+        case "POST":
+            let requestBody = '';
+
+            req.on('data', (chunk) => {
+            requestBody += chunk;
+            });
+
+            req.on('end', async () => {
+            try {
+                const jsonData = JSON.parse(requestBody);
+                
+                await mamaliaController.store(jsonData);
+
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end("Mamalia Berhasil Ditambahkan");
+            } catch (error) {
+                res.writeHead(400, { 'Content-Type': 'text/plain' });
+                res.end('Error parsing JSON data');
+            }
+            });
+            break;
+
+        default:
+            break;
     }
+});
+
+server.listen(3000, () => {
+    console.log("Server Berjalan di Port 3000");
+})
+
+// async function main() {
+//     try {
+//         const mamalia = { name: "Lumba-lumba", type: "Mamalia", habitat: "Laut" } 
+//         const mamaliaController = new MamaliaController();
+//         //mamaliaController.getSuara();
+
+//         //await mamaliaController.store(mamalia);
+
+//         const mamalias = await mamaliaController.getAll();
+//         console.log(mamalias);
+//     } catch (error) {
+//         console.log(error);
+//     }
 
     
-}
+// }
 
-main();
+//main();
